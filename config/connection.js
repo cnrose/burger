6a3af4e385f376396469400738e40abd60f53f91
Caitlin Rose <cnrose02@gmail.com>
2017-08-14 18:00:19 -0400

    updated devour functionality

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+//dependencies
+var mysql = require("mysql");
+//create connection to mysql database
+var connection = mysql.createConnection( {
+	host: "localhost",
+	user: "root",
+	password: "Basic2",
+	database: "burgers_db"
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
